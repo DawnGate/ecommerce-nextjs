616767655d6462ed426fe542ce203de0a19b87df
DawnGate <lam99quyen@gmail.com>
2022-09-26 17:27:50 +0700

    update code for footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -8,13 +8,49 @@ import { Github, Vercel } from '@components/icons'
 import { Logo, Container } from '@components/ui'
 import { I18nWidget } from '@components/common'
 import s from './Footer.module.css'
+import PagesManifestPlugin from 'next/dist/build/webpack/plugins/pages-manifest-plugin'
 interface Props {
 	className: string
 	children: any
 	pages?: Page
 }
-const Footer: FC<Props> = () => {
-	return null
+
+const links = [
+	{
+		name: 'Home',
+		url: '/',
+	},
+]
+
+function bySortOrder(a: Page, b: Page) {
+	return (a.sort_order ?? 0) - (b.sort_order ?? 0)
+}
+
+function usePages(pages?: Page[]) {
+	const { locale } = useRouter()
+	const sitePages: Page[] = []
+
+	if (pages) {
+		pages.forEach((page) => {
+			const slug = page.url && getSlug(page.url)
+			if (!slug) return
+			if (locale && !slug.startsWith(`${locale}/`)) return
+			sitePages.push(page)
+		})
+	}
+
+	return {
+		sitePages: sitePages.sort(bySortOrder),
+	}
+}
+const Footer: FC<Props> = ({ className, pages }) => {
+	const { sitePages } = usePages()
+	const rootClassName = cn(s.root, className)
+	return (
+		<footer className={rootClassName}>
+			<div></div>
+		</footer>
+	)
 }
 
 export default Footer
--- components/common/I18nWidget/I18nWidget.tsx
+++ components/common/I18nWidget/I18nWidget.tsx
@@ -36,7 +36,73 @@ const LOCALES_MAP: Record<string, LOCALE_DATA> = {
 }
 
 const I18nWidget: FC = () => {
-	return null
+	const [display, setDisplay] = useState(false)
+	const {
+		locale,
+		locales,
+		defaultLocale = 'en-US',
+		asPath: currentPath,
+	} = useRouter()
+
+	const options = locales?.filter((val) => val !== locale)
+	const currentLocale = locale || defaultLocale
+	return (
+		<ClickOutside active={display} onClick={() => setDisplay(false)}>
+			<nav className={s.root}>
+				<div
+					className="flex items-center relative"
+					onClick={() => setDisplay(!display)}
+				>
+					<button className={s.button} aria-label="Language selector">
+						<Image
+							width="20"
+							height="20"
+							className="block mr-2 w-5"
+							src={`/${LOCALES_MAP[currentLocale].img.filename}`}
+							alt={LOCALES_MAP[currentLocale].img.alt}
+							unoptimized
+						/>
+						{options && (
+							<span className="cursor-pointer">
+								<ChevronUp
+									className={cn(s.icon, { [s.active]: display })}
+								></ChevronUp>
+							</span>
+						)}
+					</button>
+				</div>
+				<div className="absolute top-0 right-0">
+					{options?.length && display ? (
+						<div className={s.dropdownMenu}>
+							<div className="flex flex-row justify-end px-6">
+								<button
+									onClick={() => setDisplay(false)}
+									aria-label="Close panel"
+									className={s.closeButton}
+								>
+									<Cross className="h-6 w-6" />
+								</button>
+							</div>
+							<ul>
+								{options.map((locale) => (
+									<li key={locale}>
+										<Link href={currentPath} locale={locale}>
+											<a
+												className={cn(s.item)}
+												onClick={() => setDisplay(false)}
+											>
+												{LOCALES_MAP[locale].name}
+											</a>
+										</Link>
+									</li>
+								))}
+							</ul>
+						</div>
+					) : null}
+				</div>
+			</nav>
+		</ClickOutside>
+	)
 }
 
 export default I18nWidget
